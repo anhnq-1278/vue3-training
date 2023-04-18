7d9038d9a399d60ef92616263a03286e438ea891
anhnq-1278 <nguyen.quoc.anh@sun-asterisk.com>
2023-04-18 10:19:45 +0700

    update some code

--- src/views/home-page.vue
+++ src/views/home-page.vue
@@ -9,20 +9,36 @@ onMounted(() => {
   lib.init()
 })
 
-async function handleDetect () {
+// async function handleDetectOpen () {
+//   try {
+//     await lib.init()
+//     console.log('init successfully')
+//     const data = await lib.open()
+//     console.log('open successfully')
+//     console.log(data)
+//   } catch(error) {
+//     console.log(error)
+//   }
+// }
+
+async function handleDetectCard () {
   try {
-    const data = await lib.open()
+    await lib.init()
+    console.log('init successfully')
+    await lib.open()
+    console.log('open successfully')
+    const data = await lib.detectCard('FeliCa')
     console.log(data)
   } catch(error) {
     console.log(error)
   }
-  
 }
 
 </script>
 
 <template>
   <div>
-    <button @click="handleDetect">detect device</button>
+    <!-- <button @click="handleDetectOpen">detect open</button> -->
+    <button @click="handleDetectCard">detect card</button>
   </div>
 </template>
