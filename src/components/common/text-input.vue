7c141b3d1099b20b20eb82375c55a7809e43c1c1
vuld-2609 <ly.duy.vu@sun-asterisk.com>
2023-04-19 17:16:32 +0700

    implement vee validate

---
+++ src/components/common/text-input.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex flex-col gap-1 mt-3">
+    <label class="text-[#45f3ff]" :for="name">{{ $attrs.label }}</label>
+    <input v-model="inputValue" :name="name" v-bind="$attrs" class="bg-[#45f3ff]" />
+    <p class="text-[#c7082e] text-xs" v-if="errorMessage">
+      {{ errorMessage }}
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useField } from 'vee-validate'
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true
+  }
+})
+
+const { value: inputValue, errorMessage } = useField(props.name, undefined, {
+  validateOnValueUpdate: false
+})
+</script>
+
+<style lang="scss" scoped>
+input {
+  outline: none;
+  border-radius: 8px;
+  height: 40px;
+  padding: 5px;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia'
 import Notifications from '@kyvg/vue3-notification'
 import App from './App.vue'
 import router from './router'
+import './validator'
 
 import './assets/main.css'
 
