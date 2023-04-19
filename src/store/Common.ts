fc1bb0c419c29a9c5924ae3c8347140e6933f2b2
phuc-2561 <duong.ngoc.phuc@sun-asterisk.com>
2023-04-19 17:04:24 +0700

    add logout

--- src/store/Common.ts
+++ src/store/Common.ts
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import type { TLogin, TRegister } from '@/model/Auth'
-import { authService } from '@/services';
+import { authService } from '@/services'
 
 export default defineStore('common', () => {
   const isLoading = ref<boolean>(false)
@@ -11,12 +11,16 @@ export default defineStore('common', () => {
   }
 
   async function login(payload: TLogin) {
-    return await authService.login(payload);
+    return await authService.login(payload)
   }
 
   async function register(payload: TRegister) {
     return await authService.register(payload)
   }
 
-  return { isLoading, setLoading, login, register }
+  function logout() {
+    localStorage.removeItem('access_token')
+  }
+
+  return { isLoading, setLoading, login, register, logout }
 })
