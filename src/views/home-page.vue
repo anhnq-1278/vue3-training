6f09bd9de307783b88947316c4373d2a8cc991a1
anhnq-1278 <nguyen.quoc.anh@sun-asterisk.com>
2023-04-19 09:07:18 +0700

    update

--- src/views/home-page.vue
+++ src/views/home-page.vue
@@ -3,9 +3,9 @@ import { onMounted, ref } from 'vue';
 // @ts-ignore
 import  { NFCPortLib } from '@/sdk/NFCPortLib.js';
 
-const cardInfo = ref<any>({})
-
 const lib = new NFCPortLib()
+const idm = ref<any>('')
+
 
 onMounted(() => {
   lib.init()
@@ -21,37 +21,22 @@ async function handleDetectCard (type: string = '') {
     console.log('open successfully')
     const data = await lib.detectCard(type)
     console.log(data)
-    cardInfo.value = data
+    getIdmFromUint8Array(data?.idm)
   } catch(error) {
     console.log(error)
   }
+}
+
+function getIdmFromUint8Array(idmData: any) {
+  idm.value = idmData.map((item: any) => item.toString(16)).join('');
 }
 
 </script>
 
 <template>
   <div>
-    <button @click="handleDetectCard()">detect card non params</button>
-    <button @click="handleDetectCard('FeliCa')">detect card FeliCa protocol</button>
-    <button @click="handleDetectCard('iso18092')">detect card iso18092 protocol</button>
-    <table v-if="Object.keys(cardInfo).length">
-      <tr>
-        <td>uid</td>
-        <td>idm</td>
-        <td>pmm</td>
-        <td>pupi</td>
-        <td>systemCode</td>
-        <td>baudRate</td>
-      </tr>
-      <tr>
-        <td>{{ cardInfo.uid }}</td>
-        <td>{{ cardInfo.idm }}</td>
-        <td>{{ cardInfo.pmm }}</td>
-        <td>{{ cardInfo.pupi }}</td>
-        <td>{{ cardInfo.systemCode }}</td>
-        <td>{{ cardInfo.baudRate }}</td>
-      </tr>
-    </table>
+    <button @click="handleDetectCard('FeliCa')">get UID card FeliCa</button>
+    <p v-if="idm.length">UID: {{ idm }}</p>
     <p v-else>no data</p>
   </div>
 </template>
@@ -69,14 +54,9 @@ button {
   margin-right: 10px;
   cursor: pointer;
 }
-table {
-  margin-top: 20px;
-  table-layout: fixed;
-  width: 100%;
-}
 
-table, th, td {
-  border: 1px solid #ccc;
+p {
+  margin-top: 20px;
 }
 
 </style>
