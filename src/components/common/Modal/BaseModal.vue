f44f673d2c232e3d91ca16ca3e6e8fa6cc96c513
duong.ngoc.phuc <SUN-ASTERISK\duong.ngoc.phuc@b122561-pc.sun-asterisk.com>
2023-04-21 17:22:13 +0700

    fix comment animation for modal

--- src/components/common/Modal/BaseModal.vue
+++ src/components/common/Modal/BaseModal.vue
@@ -1,14 +1,15 @@
 <template>
-  <!-- Render inside our `<div id="modals"></div>` in index.html -->
-  <Teleport to="#modals">
-    <div v-if="$attrs.show" class="">
-      <div class="fixed inset-0 bg-gray-900 opacity-40"></div>
-      <div class="fixed inset-0 flex items-center justify-center">
-        <div :style="{ width: props.width }" class=" text-black h-auto">
-          <slot />
+  <Teleport to="body">
+    <transition name="fade">
+      <div v-if="$attrs.show">
+        <div class="fixed inset-0 bg-gray-900 opacity-40"></div>
+        <div class="fixed inset-0 flex items-center justify-center">
+          <div :style="{ width: props.width }" class="text-black h-auto">
+            <slot />
+          </div>
         </div>
       </div>
-    </div>
+    </transition>
   </Teleport>
 </template>
 <script setup lang="ts">
@@ -19,3 +20,14 @@ const props = defineProps({
   }
 })
 </script>
+<style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
