13b138acf7affe8beb1e8e63701e5803820e4ee5
huynh.huu.hieu <>
2023-04-24 11:07:06 +0900

    change name class of comp loadingPage

--- src/components/LoadingPage.vue
+++ src/components/LoadingPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="lds-ring">
+  <div class="loading">
     <div></div>
     <div></div>
     <div></div>
@@ -8,13 +8,13 @@
 </template>
 
 <style scoped>
-.lds-ring {
+.loading {
   display: inline-block;
   position: relative;
   width: 80px;
   height: 80px;
 }
-.lds-ring div {
+.loading div {
   box-sizing: border-box;
   display: block;
   position: absolute;
@@ -26,13 +26,13 @@
   animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
   border-color: #1abc9c transparent transparent transparent;
 }
-.lds-ring div:nth-child(1) {
+.loading div:nth-child(1) {
   animation-delay: -0.45s;
 }
-.lds-ring div:nth-child(2) {
+.loading div:nth-child(2) {
   animation-delay: -0.3s;
 }
-.lds-ring div:nth-child(3) {
+.loading div:nth-child(3) {
   animation-delay: -0.15s;
 }
 @keyframes lds-ring {
