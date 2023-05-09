9925fd464d826082712c1c931912e14afd4c1d7f
duong.ngoc.phuc <SUN-ASTERISK\duong.ngoc.phuc@b122561-pc.sun-asterisk.com>
2023-05-09 14:55:31 +0700

    test template ref

--- src/views/ComponentTest.vue
+++ src/views/ComponentTest.vue
@@ -1,11 +1,23 @@
 <template>
   <FilterPopup />
   <RefTest />
+  <div>
+    <input ref="input" type="text" class="h-12 w-[50%] border border-red-400" />
+  </div>
 </template>
 <script setup lang="ts">
 import FilterPopup from '@/components/FilterPopup.vue'
 import RefTest from '@/components/RefTest.vue'
+import { onMounted, ref } from 'vue'
 
+const input = ref(null)
+
+onMounted(() => {
+  input.value.focus()
+  input.value.value = 'phuc'
+  input.value.draggable = true
+
+})
 </script>
 
 <style lang="scss" scoped>
