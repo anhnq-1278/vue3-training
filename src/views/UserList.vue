7e0ce567dcc447472c5896a861c740ef3f5dd339
duong.ngoc.phuc <SUN-ASTERISK\duong.ngoc.phuc@b122561-pc.sun-asterisk.com>
2023-05-15 16:41:05 +0700

    test pagination

--- src/views/UserList.vue
+++ src/views/UserList.vue
@@ -18,7 +18,7 @@
       </div>
     </div>
     <UserListTable :userList="userList" />
-    <Pagination
+    <!-- <Pagination
       class="mt-1 flex items-center justify-center"
       v-model="params.page"
       :total-items="metaData.totalItem"
@@ -26,7 +26,15 @@
       :slice-length="4"
       previous-label="<<<"
       next-label=">>>"
-    />
+    /> -->
+    <div class="flex items-center justify-center mt-2">
+      <PaginationVer2
+        :total-items="metaData.totalItem"
+        :items-per-page="metaData.limit"
+        :max-pages-shown="5"
+        v-model="params.page"
+      />
+    </div>
   </div>
 </template>
 <script lang="ts" setup>
@@ -44,6 +52,7 @@ import LimitSelect from '@/components/UserList/LimitSelect.vue'
 import SearchUser from '@/components/UserList/SearchUser.vue'
 import UserListTable from '@/components/UserList/UserListTable.vue'
 import Pagination from '@/components/common/Pagination/Pagination.vue'
+import PaginationVer2 from '@/components/common/PaginationV2/Pagination.vue'
 import { LIMIT_OPTIONS } from '@/constants'
 import type { TMeta, TSearchUser, TSearchUserParams } from '@/model/User'
 import type { TUser, TUserListParams } from '@/model/User'
@@ -116,4 +125,50 @@ onBeforeMount(async () => {
 
 const userSearchList = ref<TSearchUser[]>([])
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+:deep() {
+  .pagination-container {
+    display: flex;
+    justify-items: center;
+    align-items: center;
+    margin-bottom: 10px;
+    border: 1px solid #e5e7eb;
+
+    .back-button {
+      width: 44px;
+      border-top-left-radius: 1px;
+      border-bottom-left-radius: 1px;
+    }
+
+    .next-button {
+      width: 44px;
+      border-top-right-radius: 1px;
+      border-bottom-right-radius: 1px;
+      border-right-width: 0.5px;
+    }
+  }
+
+  .paginate {
+    &-buttons {
+      height: 33px;
+      width: 33px;
+      padding: 6px 12px;
+      cursor: pointer;
+      background-color: #ffffff;
+      color: #6b7280;
+      border-right: 0.5px solid #e5e7eb;
+      border-left: 0.5px solid #e5e7eb;
+    }
+
+    &:hover {
+      background-color: #d8d8d8;
+    }
+  }
+
+  .active-page {
+    background-color: #e1effe;
+    border: 1px solid #3498db;
+    color: #1c64f2;
+  }
+}
+</style>
