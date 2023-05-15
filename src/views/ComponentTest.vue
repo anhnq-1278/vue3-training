04dc08a35f2e19f7dfd3d7ad5ddbb9ee29e5d176
phuc-2561 <duong.ngoc.phuc@sun-asterisk.com>
2023-05-15 07:19:24 +0700

    fix

--- src/views/ComponentTest.vue
+++ src/views/ComponentTest.vue
@@ -1,26 +1,21 @@
 <template>
-<<<<<<< HEAD
   <div class="w-[839px] mx-auto pt-10 flex flex-col gap-10">
     <DatePickerSingle />
     <DatePickerFull />
   </div>
   <!-- <FilterPopup />
   <RefTest /> -->
-=======
   <FilterPopup />
   <RefTest />
   <div>
     <input ref="input" type="text" class="h-12 w-[50%] border border-red-400" />
   </div>
->>>>>>> 9925fd464d826082712c1c931912e14afd4c1d7f
 </template>
 <script setup lang="ts">
 import FilterPopup from '@/components/FilterPopup.vue'
 import RefTest from '@/components/RefTest.vue'
-<<<<<<< HEAD
 import DatePickerFull from '@/components/common/DatePicker/DatePickerFull.vue'
 import DatePickerSingle from '@/components/common/DatePicker/DatePickerSingle.vue'
-=======
 import { onMounted, ref } from 'vue'
 
 const input = ref(null)
@@ -31,7 +26,6 @@ onMounted(() => {
   input.value.draggable = true
 
 })
->>>>>>> 9925fd464d826082712c1c931912e14afd4c1d7f
 </script>
 
 <style lang="scss" scoped>
