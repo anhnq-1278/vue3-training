9ac574b271e05880b11071efeb3a7c399c9f752a
tiennm-3115 <nguyen.minh.tien-c@sun-asterisk.com>
2023-05-17 13:00:01 +0700

    Fix: scroll back

---
+++ src/utils/ScrollBack.ts
@@ -0,0 +1,14 @@
+import type { Ref } from 'vue'
+import { nextTick } from 'vue'
+
+//Distance props represents the distance from the element to the header.
+export default (ref: Ref, distance = 0) => {
+  //Padding of fixed header
+  const PADDING_TOP = 60
+
+  nextTick(() => {
+    const top = ref.value.getBoundingClientRect().y - PADDING_TOP - distance
+
+    window.scrollTo({ top, behavior: 'smooth' })
+  })
+}
